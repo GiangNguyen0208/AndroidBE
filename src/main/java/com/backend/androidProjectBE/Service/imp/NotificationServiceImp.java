85bb01b6a0bf30f0a8fcecc5fba87c006380c1a9
jussahug <21130377@st.hcmuaf.edu.vn>
2024-06-14 23:35:09 +0700

    notify

--- src/main/java/com/backend/androidProjectBE/Service/imp/NotificationServiceImp.java
+++ src/main/java/com/backend/androidProjectBE/Service/imp/NotificationServiceImp.java
@@ -1,9 +1,6 @@
 package com.backend.androidProjectBE.Service.imp;
 
-import com.backend.androidProjectBE.Entity.Anounces;
 import com.backend.androidProjectBE.dto.AnouncesDTO;
-import com.backend.androidProjectBE.dto.MessageDTO;
-import com.backend.androidProjectBE.dto.OrderItemDTO;
 
 import java.util.List;
 
